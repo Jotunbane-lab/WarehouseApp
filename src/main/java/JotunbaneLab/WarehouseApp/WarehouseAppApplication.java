3533ad068620b25778fe560bfdf0d72d52105af1
Jotunbane <PawMikolajczak@gmail.com>
2020-11-22 13:49:46 +0100

    Yup, relations work.

--- src/main/java/JotunbaneLab/WarehouseApp/WarehouseAppApplication.java
+++ src/main/java/JotunbaneLab/WarehouseApp/WarehouseAppApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class WarehouseAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(WarehouseAppApplication.class, args);
+		System.out.println("Starting App");
+		//SpringApplication.run(WarehouseAppApplication.class, args);
 	}
 
 }
