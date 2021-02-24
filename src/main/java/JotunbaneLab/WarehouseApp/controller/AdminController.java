b4d7496af3d70c3bc0f776c4cd3829d7b4e9b6fe
Jotunbane <PawMikolajczak@gmail.com>
2021-02-24 18:18:19 +0100

    Better Controllers

--- src/main/java/JotunbaneLab/WarehouseApp/controller/AdminController.java
+++ src/main/java/JotunbaneLab/WarehouseApp/controller/AdminController.java
@@ -13,43 +13,16 @@ import java.util.List;
 
 @Controller
 public class AdminController {
-    ClientService clientService;
 
-    public AdminController(ClientService clientService){
-        this.clientService = clientService;
-    }
+
+
 
     @GetMapping("/admin")
     public ModelAndView getAdminView() {
         return new ModelAndView("admin");
     }
 
-    @GetMapping("/admin/addClient")
-    public ModelAndView getAddClientView() {
-        return new ModelAndView("addClient", "newClient", new ClientDTO());
-    }
 
-
-    @PostMapping("/admin/addClient")
-    public String addClient(@ModelAttribute ClientDTO clientDTO) {
-        clientService.addClient(clientDTO);
-
-        return "redirect:/admin/clients";
-    }
-
-    @GetMapping("/admin/clients")
-    public ModelAndView getAllClients() {
-        List<ClientDTO> clientDTOList = clientService.getAllClients();
-        return new ModelAndView("clients", "clientList", clientDTOList);
-    }
-
-    @PostMapping("/admin/deleteClient")
-    public String deleteClient(@ModelAttribute ClientDTO clientDTO){
-        System.out.println("Usuwam klienta:"+ clientDTO.getName());
-        clientService.deleteClient(clientDTO.getId());
-
-        return "redirect:/admin/clients";
-    }
 
 
 }
---
+++ src/main/java/JotunbaneLab/WarehouseApp/controller/ClientController.java
@@ -0,0 +1,47 @@
+package JotunbaneLab.WarehouseApp.controller;
+
+import JotunbaneLab.WarehouseApp.model.dto.ClientDTO;
+import JotunbaneLab.WarehouseApp.service.ClientService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+@Controller
+public class ClientController {
+    ClientService clientService;
+
+    public ClientController(ClientService clientService){
+        this.clientService = clientService;
+    }
+
+    @GetMapping("/admin/addClient")
+    public ModelAndView getAddClientView() {
+        return new ModelAndView("addClient", "newClient", new ClientDTO());
+    }
+
+
+    @PostMapping("/admin/addClient")
+    public String addClient(@ModelAttribute ClientDTO clientDTO) {
+        clientService.addClient(clientDTO);
+
+        return "redirect:/admin/clients";
+    }
+
+    @GetMapping("/admin/clients")
+    public ModelAndView getAllClients() {
+        List<ClientDTO> clientDTOList = clientService.getAllClients();
+        return new ModelAndView("clients", "clientList", clientDTOList);
+    }
+
+    @PostMapping("/admin/deleteClient")
+    public String deleteClient(@ModelAttribute ClientDTO clientDTO){
+        System.out.println("Usuwam klienta:"+ clientDTO.getName());
+        clientService.deleteClient(clientDTO.getId());
+
+        return "redirect:/admin/clients";
+    }
+}
